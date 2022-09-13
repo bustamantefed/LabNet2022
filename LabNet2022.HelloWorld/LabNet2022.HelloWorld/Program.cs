c1f0a6ef1d4bbd21695ded56b774f7cc90a5240e
bustamantefed@gmail.com <bustamantefed@gmail.com>
2022-09-13 00:16:41 -0300

    Se sube primer ejercicio y proyecto HelloWorld.
    Se sube código junto con archivos de proyecto y solucion de HelloWorld.

---
+++ LabNet2022.HelloWorld/LabNet2022.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabNet2022.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
